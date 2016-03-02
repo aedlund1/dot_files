78da6380f469cc7691589e2a4c7e29fc1721e845
Aren Edlund <aedlund1@jaguarlandrover.com>
2016-03-02 15:43:41 -0800

    [master] addition of more keymaps in xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,6 +5,12 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
+
+cmd_lock = "slock"
+cmd_volDown = "amixer set Master 2-"
+cmd_volUp = "amixer set Master 2+"
+cmd_volMute = "amixer set Master toggle"
+
 main = do
     xmproc <- spawnPipe "/home/aren/.cabal/bin/xmobar /home/aren/.xmobarrc"
     xmonad $ defaultConfig
@@ -16,7 +22,10 @@ main = do
                         }
         , modMask = mod4Mask
         } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensave-command -lock")
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn cmd_lock)
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
+        , ((mod4Mask .|. shiftMask, xK_Up), spawn cmd_volUp)
+        , ((mod4Mask .|. shiftMask, xK_Down), spawn cmd_volDown)
+        , ((mod4Mask .|. shiftMask, xk_Left), spawn cmd_volMute)
         ]
