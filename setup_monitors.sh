a292cb7390e7b873ee84a3d2483956be9f7994a4
Aren Edlund <aedlund1@jaguarlandrover.com>
2016-04-25 10:08:40 -0700

    [master] Setup monitors failed in previous order

--- setup_monitors.sh
+++ setup_monitors.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-xrandr --output DP-1 --auto --right-of DP-2 
 xrandr --output DP-2 --auto --rotate left 
+xrandr --output DP-1 --auto --right-of DP-2 
 feh --bg-fill /home/$USER/images/background.jpg
