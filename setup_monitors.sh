53e598bdc494179130ecaf8979420f56c715c269
Aren Edlund <aedlund1@jaguarlandrover.com>
2016-06-16 14:14:19 -0700

    [master] fix typo in setup monitors

--- setup_monitors.sh
+++ setup_monitors.sh
@@ -2,6 +2,5 @@
 
 xrandr --output DP-2 --auto --rotate left 
 xrandr --output DP-1 --auto --right-of DP-2 
-find /home/$USER/Images/backgrounds/ -type f -name "*.jpg" | sort -R | tail -n 1 | while read file; do
-    feh --bg-fill $file
-done
+feh --bg-center --randomize /home/$USER/Images/backgrounds/* 
+#done
