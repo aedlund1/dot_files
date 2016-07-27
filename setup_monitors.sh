d7c196b165ad27d47eb8c497a93669584a411749
Aren Edlund <aedlund1@jaguarlandrover.com>
2016-07-27 15:37:35 -0700

    [master] add second xmobar

--- setup_monitors.sh
+++ setup_monitors.sh
@@ -2,5 +2,6 @@
 
 xrandr --output DP-2 --auto
 xrandr --output DP-1 --auto --right-of DP-2 
+xrandr --output DVI-I-1 --auto --right-of DP-1
 feh --bg-center --randomize /home/$USER/Images/backgrounds/* 
 #done
